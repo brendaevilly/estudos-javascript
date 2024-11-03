83f5a10454ae14fea646f2640da27ee9c49496ef
Brenda Evilly <165739999+brendaevilly@users.noreply.github.com>
2024-11-03 10:42:49 -0300

    novos exercícios

---
+++ aula11/ex010.js
@@ -0,0 +1,7 @@
+var velocidade = 90
+console.log(`Sua velocidade é de ${velocidade} km/h`)
+if(velocidade > 60){
+    console.log(`Você ultrapaçou a velocidade permitida!`)
+}else{
+    console.log(`Você está na velocidade correta!`)
+}
---
+++ aula11/ex011.js
@@ -0,0 +1,7 @@
+var pais = 'China'
+
+if(pais == 'Brasil'){
+    console.log('Você é brasileiro!')
+}else{
+    console.log('Você é estrangeiro!')
+}
